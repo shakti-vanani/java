34bcb08e989a039f95f8658528ae8fac410eb014
parmar-viral <parmarviral8324@gmail.com>
2023-11-15 15:31:59 -0800

    datatype.java add

---
+++ datatype.java
@@ -0,0 +1,33 @@
+public class datatype {
+    public static void main(String[] args) {
+        //int datatype
+        int i=1000;
+        System.out.println("value of i="+i);
+        //char datatype
+        char c='A';
+        System.out.println("value of c="+c);
+        //float datatype
+        float f=112.22F;  //This data type ends with ‘f’ or ‘F’.
+        System.out.println("value of f="+f);         
+        //double datatype
+        double d=12.121314;
+        System.out.println("value of d=" + d);
+        //long datatype
+        long l=2212134l;  //This data type ends with ‘L’ or ‘l’.
+        System.out.println("value of l="+l);
+        //boolean datatype
+        boolean b=true;
+        if (b==true) {
+            System.out.println("boolean value is true");
+        }
+        else{
+            System.out.println("boolean value is false");
+        }
+        //byte datatype
+        byte b1=10;
+        System.out.println("value of b="+b1);
+        //short datatype
+        short s=5;
+        System.out.println("value of s="+s);
+    }
+}
