607847ba344c5d15f2692d0e8a8e66880a2e6816
parmar-viral <parmarviral8324@gmail.com>
2023-11-16 08:03:24 -0800

    left shift operator

---
+++ leftshift_operator.java
@@ -0,0 +1,8 @@
+public class leftshift_operator {
+    public static void main(String[] args) {
+        int originalval=10; //binary value:1010
+        int shiftedval=originalval<<2; //binary value: 101000(decimal:40)
+        System.out.println("originalvalue="+originalval);
+        System.out.println("shidtedvalue="+shiftedval);
+    }
+}
