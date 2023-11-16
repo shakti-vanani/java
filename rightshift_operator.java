db723e94d1ec99265457b762265a655bcb925d09
parmar-viral <parmarviral8324@gmail.com>
2023-11-16 08:43:17 -0800

    rightshift operator

---
+++ rightshift_operator.java
@@ -0,0 +1,8 @@
+public class rightshift_operator {
+    public static void main(String[] args) {
+        int originalval=50;
+        int shidtedvalue=originalval>>2;
+        System.out.println("original value="+originalval);
+        System.out.println("shifted value="+shidtedvalue);
+    }
+}
