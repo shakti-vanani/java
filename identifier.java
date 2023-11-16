9975c9d9dad422a877a1ef7e22c8f92568a30be2
parmar-viral <parmarviral8324@gmail.com>
2023-11-15 17:16:38 -0800

    identifier.java and key.java add

---
+++ identifier.java
@@ -0,0 +1,9 @@
+public class identifier {
+    public static void main(String[] args) {
+        float r = 12.01f;
+        float area;
+        area = 3.1416f * r * r;
+        System.out.println("area of circle="+area);
+
+    }
+}
---
+++ key.java
@@ -0,0 +1,10 @@
+public class key {
+    public static void main(String[] args) {
+        int no=10;
+        if (no > 0) {
+             System.out.println("no is positive");
+        } else {
+            System.out.println("no is negative");
+        }
+    }
+}
