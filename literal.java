3e0e595b39f5a97f433e45b7a9998f9004177282
parmar-viral <parmarviral8324@gmail.com>
2023-11-15 19:20:58 -0800

    literal.java add

---
+++ literal.java
@@ -0,0 +1,18 @@
+public class literal {
+    public static void main(String[] args) {
+    int intval = 42;
+    double doubleval = 3.14;
+    char charVal = 'A';
+    String stringval = "Hello";
+    boolean boolVal = true;
+    float floatval=12.10f;
+    System.out.println("intvalue="+intval);
+    System.out.println("floatvalue="+floatval);
+    System.out.println("doublevalue="+doubleval);
+    System.out.println("charvalue="+charVal);
+    System.out.println("stringvalue="+stringval);
+    System.out.println("boolvalue="+boolVal);
+    }
+   
+}
+
