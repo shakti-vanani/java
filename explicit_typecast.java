dd8ff184e213fc79f566256f9ed7b4b3f0ae3a69
parmar-viral <parmarviral8324@gmail.com>
2023-11-17 06:09:32 -0800

    explicit type casting

---
+++ explicit_typecast.java
@@ -0,0 +1,13 @@
+/**
+ * explicit_typecast
+ */
+public class explicit_typecast {
+    public static void main(String[] args) {
+        //explicit type casting 
+        double d = 112.23;
+        int i = (int) d; // double is explicitly cast to int
+        System.out.println("double d="+d);
+        System.out.println("int i="+i);
+    }
+    
+}
