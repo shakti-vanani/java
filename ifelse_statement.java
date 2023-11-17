82bccb3e052b871df931e65f4b6b36e323498a07
parmar-viral <parmarviral8324@gmail.com>
2023-11-17 07:33:58 -0800

    if-else statement

---
+++ ifelse_statement.java
@@ -0,0 +1,11 @@
+public class ifelse_statement {
+
+    public static void main(String[] args){
+        int no = 50;
+        if (no%2==0) {
+            System.out.println("no is even");
+        } else {
+            System.out.println("no is odd");
+        }
+    }
+}
