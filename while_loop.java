dea61790d992537c784ea39c0af2b90c533b75a7
parmar-viral <parmarviral8324@gmail.com>
2023-11-17 08:05:35 -0800

    while loop

---
+++ while_loop.java
@@ -0,0 +1,10 @@
+public class while_loop {
+    public static void main(String[] args) {
+        int no=19;
+        int count = 1;
+        while (count <= 10) {
+          System.out.println(no+"*"+count+"="+no*count);
+          count++;
+        }
+    }
+}
