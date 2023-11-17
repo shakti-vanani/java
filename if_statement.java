ddd4835e513730fa13363b589f037f04fae3ac08
parmar-viral <parmarviral8324@gmail.com>
2023-11-17 06:45:05 -0800

    if statement

---
+++ if_statement.java
@@ -0,0 +1,8 @@
+public class if_statement {
+    public static void main (String[] args){
+        int age = 20;
+        if (age > 0) {
+        System.out.println("value of age is="+age);
+    }
+}
+}
