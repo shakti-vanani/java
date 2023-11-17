014b3acabd88ed87a2d9f441ffd006ceeeac1928
parmar-viral <parmarviral8324@gmail.com>
2023-11-17 04:46:38 -0800

    assert keyword in java and implicit type casting

---
+++ assert_keyword.java
@@ -0,0 +1,7 @@
+public class assert_keyword {
+    public static void main(String[] args) {
+       int value=10;
+       assert value==10:"value should be 10";
+        System.out.println();
+    }
+}
---
+++ implicit_typecast.java
@@ -0,0 +1,8 @@
+public class implicit_typecast {
+    public static void main(String[] args) {
+        // Implicit Type Casting Example
+        int i = 102;
+        double d=i; // int is implicitly cast to double
+        System.out.println("int i="+i+"\n"+"double d="+d);
+    }
+}
