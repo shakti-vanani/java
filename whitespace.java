7ea72a45a203ef6127b115eb3cf0d6181e41ed3f
parmar-viral <parmarviral8324@gmail.com>
2023-11-23 04:37:25 -0800

    whitespace in java

---
+++ whitespace.java
@@ -0,0 +1,11 @@
+public class whitespace {
+    public static void main(String[] args) {
+        int x = 5; // Declaration and initialization of variable x
+        int y = 10; // Declaration and initialization of variable y
+
+        // Performing a simple calculation
+        int sum = x + y;
+        // Displaying the result
+        System.out.println("The sum of x and y is: " + sum);
+    }
+}
