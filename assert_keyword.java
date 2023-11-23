3adf6390d2cec6468a4789078eac9e43a4600ed7
parmar-viral <parmarviral8324@gmail.com>
2023-11-22 20:16:16 -0800

    assert program edit

--- assert_keyword.java
+++ assert_keyword.java
@@ -1,7 +1,7 @@
 public class assert_keyword {
     public static void main(String[] args) {
-       int value=10;
-       assert value==10:"value should be 10";
-        System.out.println();
+        int value = 10;
+        assert value > 0 : "Value should be positive";
+        System.out.println("Value is: " + value);
     }
 }
