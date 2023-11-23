8cc7d1740ccef1848032684af6fd251083cd2968
parmar-viral <parmarviral8324@gmail.com>
2023-11-23 13:57:05 -0800

    iib example

---
+++ IIBExample.java
@@ -0,0 +1,15 @@
+public class IIBExample {
+    // Instance Initialization Block
+    {
+        System.out.println("Instance Initialization Block executed");
+    }
+
+    // Constructor
+    public IIBExample() {
+        System.out.println("Constructor executed");
+    }
+
+    public static void main(String[] args) {
+        new IIBExample();
+    }
+}
