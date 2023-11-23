816de8d98a4305b6497b7d24de43d64d0809882d
parmar-viral <parmarviral8324@gmail.com>
2023-11-23 04:51:24 -0800

    modifier in java

---
+++ modifier.java
@@ -0,0 +1,24 @@
+public class modifier {
+    // Public variable accessible from outside the class
+    public int publicVariable = 10;
+    // private variable accessible only within the class
+    private int i=20;
+    // Public method accessible from outside the class
+    public void publicMethod() {
+        System.out.println("This is a public method."+publicVariable);
+    }
+     // Private method accessible only within the class
+    public void privateMethod(){
+        System.out.println("This is a private Method."+i);
+    }
+    public static void main(String[] args) {
+        modifier obj = new modifier();
+        // Accessing public members
+        System.out.println("Public Variable: " + obj.publicVariable);
+        obj.publicMethod();
+        // Accessing private members (not allowed outside the class)
+        // System.out.println("Private Variable: " + obj.privateVariable); //
+        // Compilation error
+        // obj.privateMethod(); // Compilation error
+    }
+}
