4ce87f7d158d9e83ae62356dd819e2a6e12505dd
parmar-viral <parmarviral8324@gmail.com>
2023-11-23 04:28:36 -0800

    comments in java

---
+++ comments.java
@@ -0,0 +1,12 @@
+public class comments {
+    public static void main(String[] args) {
+        // This is a single-line comment
+        System.out.println("Hello, Java!"); // This is also a single-line comment
+        /*
+          This is a multi-line comment.
+          It can span multiple lines.
+         */
+        // Using comments to disable code temporarily
+        // System.out.println("This line won't be executed.");
+    }
+}
