4282e62e5167425f760622b06d3ac4485274f621
viral-parmar <parmarviral8324@gmail.com>
2023-11-26 06:30:34 +0530

    java lang package example

---
+++ java_lang_example.java
@@ -0,0 +1,8 @@
+public class java_lang_example{
+    public static void main(String[] args) {
+        String Message1 = "Hello, Java!";
+        String Message2="Java Programming!";
+        System.out.println(Message1);
+        System.out.println(Message2);
+    }
+}
